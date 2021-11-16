d38e2451aa4ffb6e9de9f6c8b3db33b0891f4882
Carl Åstholm <carl.astholm@visibacare.com>
2021-11-16 09:56:26 +0100

    Try different loading method

--- src/main.ts
+++ src/main.ts
@@ -10,20 +10,37 @@ app.ticker.minFPS = 15
 app.ticker.maxFPS = 60
 document.body.appendChild(app.view)
 
-const sprites = [
-    PIXI.Sprite.from("assets/VC_Care.png"),
-    PIXI.Sprite.from("assets/VC_Act.png"),
-    PIXI.Sprite.from("assets/VC_Grow.png"),
-    PIXI.Sprite.from("assets/Red_Robin.png")
+const sprites: PIXI.Sprite[] = [
+    // PIXI.Sprite.from("assets/VC_Care.png"),
+    // PIXI.Sprite.from("assets/VC_Act.png"),
+    // PIXI.Sprite.from("assets/VC_Grow.png"),
+    // PIXI.Sprite.from("assets/Red_Robin.png")
 ]
 
-for (const sprite of sprites) {
-    sprite.anchor.set(0.5)
-    sprite.scale.set(0.25)
-    app.stage.addChild(sprite)
-}
+// for (const sprite of sprites) {
+//     sprite.anchor.set(0.5)
+//     sprite.scale.set(0.25)
+//     app.stage.addChild(sprite)
+// }
 
 let t = 0
+app.loader
+    .add("care", "assets/VC_Care.png")
+    .add("act", "assets/VC_Act.png")
+    .add("grow", "assets/VC_Grow.png")
+    .add("redRobin", "assets/Red_Robin.png")
+    .load((loader, resources) => {
+        sprites.push(new PIXI.Sprite(resources["care"]!.texture!))
+        sprites.push(new PIXI.Sprite(resources["act"]!.texture!))
+        sprites.push(new PIXI.Sprite(resources["grow"]!.texture!))
+        sprites.push(new PIXI.Sprite(resources["redRobin"]!.texture!))
+
+        for (const sprite of sprites) {
+            sprite.anchor.set(0.5)
+            sprite.scale.set(0.25)
+            app.stage.addChild(sprite)
+        }
+    })
 app.ticker.add(dt => {
     t += dt
     for (let i = 0; i < sprites.length; i++) {
